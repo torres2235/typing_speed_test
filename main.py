49d2b7ba0392c2c810895ac8b3c3a37aae03f552
Torres2235 <Torres2235@gmail.com>
2024-08-29 13:41:10 -0700

    using Label instead of Canvas

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from tkinter import *
-#from tkinter import ttk
 import pandas as pd
 import random
+import time
 
 window = Tk()
 window.title("Typing Speed Test")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(width=350, height=200)
+# replace with pandas eventually #
+paragraphs = []
 with open("data/paragraphs.txt", "r") as data:
-    paragraphs = []
     for line in data:
         paragraphs.append(line)
-    canvas.create_text(175, 100, text=paragraphs[random.randint(0, len(paragraphs))], font=("Ariel", 20))
-    canvas.pack()
-canvas.grid(row=0, column=0)
+# ------------------------------ #
+text = Label(text=paragraphs[random.randint(0, len(paragraphs))], font=("Ariel", 25), wraplength=800, justify="center")
+text.grid(row=0, column=0)
 
 text = Text(width=50,
             height=5,
