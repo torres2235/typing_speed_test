78138a0d5501eb744ec115012264d0f14f8d7606
Torres2235 <Torres2235@gmail.com>
2024-08-29 13:10:27 -0700

    adding list of paragraphs and more work on main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from tkinter import *
-from tkinter import ttk
+#from tkinter import ttk
+import pandas as pd
+import random
 
 window = Tk()
 window.title("Typing Speed Test")
 window.config(padx=50, pady=50)
 
+canvas = Canvas(width=350, height=200)
+with open("data/paragraphs.txt", "r") as data:
+    paragraphs = []
+    for line in data:
+        paragraphs.append(line)
+    canvas.create_text(175, 100, text=paragraphs[random.randint(0, len(paragraphs))], font=("Ariel", 20))
+    canvas.pack()
+canvas.grid(row=0, column=0)
 
-user_input = StringVar()
-input_entry = ttk.Entry(width=21)
-input_entry.grid(column=1, row=1)
+text = Text(width=50,
+            height=5,
+            padx=10,
+            pady=10
+            )
+text.grid(row=1, column=0)
 
 window.mainloop()
