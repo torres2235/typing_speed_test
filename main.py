8c0c782c6bb246b3a02c2ad4b97b770db90a406f
Torres2235 <Torres2235@gmail.com>
2024-11-26 15:52:42 -0800

    adding function and button to change paragraph

--- main.py
+++ main.py
@@ -7,20 +7,53 @@ window = Tk()
 window.title("Typing Speed Test")
 window.config(padx=50, pady=50)
 
+canvas = Canvas(window, width=1000, height=200)
+
 # replace with pandas eventually #
 paragraphs = []
 with open("data/paragraphs.txt", "r") as data:
     for line in data:
         paragraphs.append(line)
 # ------------------------------ #
-text = Label(text=paragraphs[random.randint(0, len(paragraphs))], font=("Ariel", 25), wraplength=800, justify="center")
-text.grid(row=0, column=0)
+curr_paragraph = paragraphs[random.randint(0, len(paragraphs)-1)]
+print(curr_paragraph)
+full_paragraph = []
+t = Text(window, fg='#808080', font=("Ariel", 25,), width=50, height=10)
+t.insert(INSERT, curr_paragraph)
+t.config(state=DISABLED)
+# for i in range(len(curr_paragraph)):
+#     #print(char)
+#     text = Label(canvas, text=curr_paragraph[i], fg='#808080', font=("Ariel", 25,), wraplength=800, justify="center")
+#     full_paragraph.append(text)
+#     text.pack(side='left')
+t.grid(row=0, column=0)
 
-text = Text(width=50,
-            height=5,
-            padx=10,
-            pady=10
-            )
-text.grid(row=1, column=0)
+
+#print(full_paragraph[0])
+
+user_input = Text(width=100,
+                  height=5,
+                  padx=10,
+                  pady=10
+                  )
+user_input.grid(row=1, column=0)
+
+def new_para():
+    curr_paragraph = paragraphs[random.randint(0, len(paragraphs) - 1)]
+    t.config(state=NORMAL)
+    t.delete("1.0", END)
+    t.insert(INSERT, curr_paragraph)
+    t.config(state=DISABLED)
+    print("displaying new paragraph")
+    print(curr_paragraph)
+
+next_btn = Button(
+    text="New Paragraph",
+    command=new_para
+                  )
+next_btn.grid(row=3, column=0)
+
+#print(text['text'][0])
+#t[0].config(fg='blue')
 
 window.mainloop()
