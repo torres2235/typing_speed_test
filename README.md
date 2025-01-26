bfd625885764c4e7c0c2026a80f05a9f9ffa9294
Torres2235 <Torres2235@gmail.com>
2025-01-25 20:23:14 -0800

    added wpm calculator

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # typing_speed_test
 A Python GUI application to help test your typing speed
-
-pip install pynput needed
-
-
-I had to check Pycharm in all three of the settings below (MacOS):
-
-    System Settings -> Privacy and Security -> Input Monitoring
-    System Settings -> Privacy and Security -> Accessibility
-    System Settings -> Privacy and Security -> Full Disk Access
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pynput import keyboard
 curr_paragraph = None
 timer = None
 mistakes = 0
+mistake_index = []
 
 
 # ----------------  Functions  ----------------#
@@ -27,13 +28,14 @@ def new_para():
     print(curr_paragraph)
     if timer:
         window.after_cancel(timer)
-    start_timer()
+    start()
 
 
-def start_timer():
+def start():
     timer_text.config(text="00:00")
     count_timer(00)
     change_color()
+    calc_wpm(00)
 
 
 def count_timer(time):
@@ -49,10 +51,9 @@ def count_timer(time):
     timer = window.after(1000, count_timer, time + 1)
 
 
-
-
 def change_color():
     global mistakes
+    global mistake_index
     display_font = font.Font(display_text, display_text.cget("font"))
     display_text.tag_config("red", font=display_font, foreground="Red")
     display_text.tag_config("green", font=display_font, foreground="Green")
@@ -62,21 +63,32 @@ def change_color():
     usr = user_input.get(1.0, "end")
 
     for i in range(len(usr)-1):
+        display_text.tag_remove("grey_bg", f"1.{i}")
         if usr[i] != dis[i]:
-            mistakes += 1
-            display_text.tag_remove("grey_bg", f"1.{i}")
+            if mistake_index.count(i) < 1:
+                mistake_index.append(i)
+                mistakes += 1
+                mistake_count.config(text=f"Mistakes: {mistakes}")
             display_text.tag_add("red", f"1.{i}")
-            mistake_count.config(text=f"Mistakes: {mistakes}")
         else:
-            display_text.tag_remove("grey_bg", f"1.{i}")
             display_text.tag_add("green", f"1.{i}")
 
     for tag in display_text.tag_names():
-        display_text.tag_remove(tag, f"1.{len(usr)}", "end")
-    window.after(1, change_color)
+        display_text.tag_remove(tag, f"1.{len(usr)-1}", "end")
 
     display_text.tag_add("grey_bg", f"1.{len(usr)-1}")
 
+    window.after(1, change_color)  # refresh
+
+
+def calc_wpm(time):
+    # wpm = (chars / 5) / mins
+    if time > 0:
+        usr = user_input.get(1.0, "end")
+        wpm = round((len(usr) / 5) / (time/60), 2)
+        wpm_count.config(text=f"WPM:{wpm}")
+
+    window.after(1000, calc_wpm, time + 1)
 
 # ----------------  Window  ----------------#
 window = Tk()
@@ -90,10 +102,12 @@ with open("data/paragraphs.txt", "r") as data:
         paragraphs.append(line)
 # ------------------------------ #
 
+wpm_count = Label(text="WPM:0.0", fg="white", font=("Arial", 35, "bold"))
+wpm_count.grid(row=0, column=0)
 timer_text = Label(text="00:00", fg="white", font=("Arial", 35, "bold"))
-timer_text.grid(column=0, row=0)
-mistake_count = Label(text="Mistakes: 0", fg="white", font=("Arial", 35, "bold"))
-mistake_count.grid(row=0, column=1)
+timer_text.grid(column=1, row=0)
+mistake_count = Label(text="Mistakes:0", fg="white", font=("Arial", 35, "bold"))
+mistake_count.grid(row=0, column=2)
 
 curr_paragraph = paragraphs[random.randint(0, len(paragraphs) - 1)]
 print(curr_paragraph)
@@ -107,7 +121,7 @@ display_text = Text(window,
                     )
 display_text.insert(INSERT, curr_paragraph)
 display_text.config(state=DISABLED)
-display_text.grid(row=1, column=0)
+display_text.grid(row=1, column=1)
 
 user_input = Text(width=100,
                   height=5,
@@ -115,13 +129,9 @@ user_input = Text(width=100,
                   pady=10,
                   wrap=WORD,
                   )
-user_input.grid(row=2, column=0)
+user_input.grid(row=2, column=1)
 
 next_btn = Button(text="New Paragraph", command=new_para)
-next_btn.grid(row=3, column=0)
-
-# listener = keyboard.Listener(
-#     on_press=change_color)  # run change_color() on keypress
-# listener.start()
+next_btn.grid(row=3, column=1)
 
 window.mainloop()
